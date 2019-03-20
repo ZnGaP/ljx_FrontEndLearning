f2f589006605d23996a658e08993c87ffdf05ade
ZnGaP <1102114189@qq.com>
2019-03-20 23:48:33 +0800

    20190320

---
+++ 784.letter-case-permutation.js
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=784 lang=javascript
+ *
+ * [784] Letter Case Permutation
+ *
+ * https://leetcode.com/problems/letter-case-permutation/description/
+ *
+ * algorithms
+ * Easy (55.49%)
+ * Total Accepted:    39.8K
+ * Total Submissions: 71.6K
+ * Testcase Example:  '"a1b2"'
+ *
+ * Given a string S, we can transform every letter individually to be lowercase
+ * or uppercase to create another string.  Return a list of all possible
+ * strings we could create.
+ * 
+ * 
+ * Examples:
+ * Input: S = "a1b2"
+ * Output: ["a1b2", "a1B2", "A1b2", "A1B2"]
+ * 
+ * Input: S = "3z4"
+ * Output: ["3z4", "3Z4"]
+ * 
+ * Input: S = "12345"
+ * Output: ["12345"]
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * S will be a string with length between 1 and 12.
+ * S will consist only of letters or digits.
+ * 
+ * 
+ */
+/**
+ * @param {string} S
+ * @return {string[]}
+ */
+var letterCasePermutation = function(S) {
+    let permutation = [];
+    
+};
+
