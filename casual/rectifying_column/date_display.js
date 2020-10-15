5b59ffac6a02799464d29585b675a31b4eeb5433
ZnGaP <871528607@qq.com>
2020-10-15 19:40:01 +0800

    2020.10.15

---
+++ casual/rectifying_column/date_display.js
@@ -0,0 +1,20 @@
+function liquid_component_display() {
+    
+}
+
+function vapor_component_display() {
+
+}
+
+function liquid_mass_display() {  
+
+}
+
+function vapor_mass_display() {  
+
+}
+
+function temperature() {  
+
+}
+
---
+++ casual/rectifying_column/js/model1.js
@@ -0,0 +1,6 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspecitveCamera(45, window.innerWidth/window.innerHeight, 1, 1000);
+var renderer = new THREE.WebGLRenderer()
+
+
+var loader = new THREE.ObjectLoader();
